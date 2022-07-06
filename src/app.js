da791e277b65f36008f3fc2ca3e6ee5ae8cf52f0
maskatK <69219044+maskatK@users.noreply.github.com>
2022-07-06 14:52:35 +0300

    Update multyplice

--- src/app.js
+++ src/app.js
@@ -13,6 +13,45 @@ function formatDate(timestamp) {
     return `${day} ${hours}:${minutes}`;
 }
 
+function displeyForecast(response) {
+    console.log(response.data.daily);
+    let forecastElement = document.querySelector("#forecast");
+    let forecastHTML = `<div class="row">`;
+    let days = ["Thu", "Fri", "Sat", "Sun"];
+    
+    days.forEach(function(day) {
+        forecastHTML = forecastHTML + `
+    
+        <div class="col-2">
+            <div class="weather-forecast-date">
+            ${day}
+            </div>
+                <img 
+                    src="http://openweathermap.org/img/wn/10d@2x.png" 
+                    alt="" 
+                    width="42"
+                />
+            <div class="weather-forecast-temperature">
+                <span class="weather-forecast-temperature-max">
+                    18º 
+                </span>
+                <span class="weather-forecast-temperature-min">
+                    12º
+                </span>
+            </div>
+        </div>`;
+    })
+    
+        forecastHTML = forecastHTML +`</div>`
+    forecastElement.innerHTML = forecastHTML;
+}
+
+function getForecast(coordinates) {
+    let apiKey="d14a5f9f0440a82a056dfd79e5c778de";
+    let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
+axios.get(apiUrl).then(displeyForecast);
+}
+
 function displeyTemperature(response) {
 let temperatureElement = document.querySelector(`#temperature`);
 let cityElement = document.querySelector(`#city`);
@@ -33,6 +72,8 @@ dateElement.innerHTML = formatDate(response.data.dt * 1000);
 iconElement.setAttribute("src", `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`);
 iconElement.setAttribute(
     "alt", response.data.weather[0].description);
+    
+    getForecast(response.data.coord);
 }
 
 function search(city) {
@@ -68,6 +109,7 @@ function showCelcius(event) {
 }
 
 let celciusTemperature = null;
+
 
 let form = document.querySelector("#serch-form");
 form.addEventListener("submit", hendleSubmit);
